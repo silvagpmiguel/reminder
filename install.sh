e6364727a9e53008913d4b8b431955aed9ebd8ea
miguel.silva <miguel.silva@vilt-group.com>
2020-11-02 11:03:08 +0000

    fix uninstall/install

--- install.sh
+++ install.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-set -e;
-
-FROM_THEMES_FOLDER=/reminder/themes/theme/Yaru-Grey-dark;
-TO_THEMES_FOLDER=/usr/share/themes/Yaru-colors/;
-FROM_ICONS_FOLDER=/reminder/themes/icons/Yaru-Grey;
-TO_ICONS_FOLDER=/usr/share/icons/Yaru-colors/;
-BIN_FOLDER=~/bin
 INSTALL_FOLDER=~/bin/reminder
+FROM_THEMES_FOLDER=$INSTALL_FOLDER/themes/theme/Yaru-Grey-dark
+TO_THEMES_FOLDER=/usr/share/themes/Yaru-colors/
+FROM_ICONS_FOLDER=$INSTALL_FOLDER/themes/icons/Yaru-Grey
+TO_ICONS_FOLDER=/usr/share/icons/Yaru-colors/
+BIN_FOLDER=~/bin
 
 echo "# Beginning Reminder Installation!"
-echo "## Verifying Yad Installation";
-YAD_OK=$(dpkg-query -W --showformat='${Status}\n' ya);
+echo "## Verifying Yad Installation"
+YAD_OK=$(dpkg-query -W --showformat='${Status}\n' yad)
+
 if [ "" = "$YAD_OK" ]; then
-    echo "No. Installing yad...";
-    sudo apt-get install yad;
+    echo "No. Installing yad..."
+    sudo apt-get update -y && sudo apt-get install -y yad
 else
-    echo "Yes";
+    echo "Yes"
 fi
+
+set -e
 
 echo "## Setting up Reminder Environment"
 mkdir -p $INSTALL_FOLDER
 echo "Moving files to /reminder..."
-mv remind $INSTALL_FOLDER
-mv awk-reminder $INSTALL_FOLDER
-mv install.sh $INSTALL_FOLDER
-mv README.txt $INSTALL_FOLDER
-mv reminder.properties $INSTALL_FOLDER
-mv rremind $INSTALL_FOLDER
-mv themes $INSTALL_FOLDER
+mv -n remind $INSTALL_FOLDER/
+mv -n awk-reminder $INSTALL_FOLDER/
+mv -n uninstall.sh $INSTALL_FOLDER/
+mv -n README.md $INSTALL_FOLDER/
+mv -n reminder.properties $INSTALL_FOLDER/
+mv -n rremind $INSTALL_FOLDER/
+mv -n themes $INSTALL_FOLDER/
 touch $INSTALL_FOLDER/reminders.txt
 echo "Moving to install folder..."
 cd $INSTALL_FOLDER
@@ -37,18 +38,18 @@ chmod +x remind awk-reminder rremind
 
 echo "## Installing Reminder Default Theme"
 echo "Creating icons/themes folders..."
-sudo mkdir $TO_THEMES_FOLDER
-sudo mkdir $TO_ICONS_FOLDER
+sudo mkdir -p $TO_THEMES_FOLDER
+sudo mkdir -p $TO_ICONS_FOLDER
 echo "Adding Yaru-Grey-dark theme..."
 sudo cp -r $FROM_THEMES_FOLDER $TO_THEMES_FOLDER
 sudo cp -r $FROM_ICONS_FOLDER $TO_ICONS_FOLDER
 
 echo "## Configuring Bashrc & Setting Symbolic Links"
-echo "Adding Non-network local connections to access control list...";
-printf "\nxhost local:$USER > /dev/null\n" >> ~/.bashrc;
-echo "Adding symbolic links for remind and rremind...";
+echo "Adding Non-network local connections to access control list..."
+printf "\nxhost local:$USER > /dev/null\n" >> ~/.bashrc
+echo "Adding symbolic links for remind and rremind..."
 ln -s $INSTALL_FOLDER/remind $BIN_FOLDER/remind
 ln -s $INSTALL_FOLDER/rremind $BIN_FOLDER/rremind
 echo "Refreshing bashrc..."
 . ~/.bashrc
-echo "# Reminder Installation Complete!";
+echo "# Reminder Installation Complete!"
--- uninstall.sh
+++ uninstall.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-FROM_THEMES_FOLDER=/reminder/themes/theme/Yaru-Grey-dark;
-TO_THEMES_FOLDER=/usr/share/themes/Yaru-colors/;
-FROM_ICONS_FOLDER=/reminder/themes/icons/Yaru-Grey;
-TO_ICONS_FOLDER=/usr/share/icons/Yaru-colors/;
-BIN_FOLDER=~/bin
 INSTALL_FOLDER=~/bin/reminder
+FROM_THEMES_FOLDER=$INSTALL_FOLDER/themes/theme/Yaru-Grey-dark
+TO_THEMES_FOLDER=/usr/share/themes/Yaru-colors/
+FROM_ICONS_FOLDER=$INSTALL_FOLDER/themes/icons/Yaru-Grey
+TO_ICONS_FOLDER=/usr/share/icons/Yaru-colors/
+BIN_FOLDER=~/bin
 
 echo "# Beginning Reminder Installation!"
-echo "## Verifying Yad Installation";
-YAD_OK=$(dpkg-query -W --showformat='${Status}\n' ya);
-if [ "" != "$YAD_OK" ]; 
-    echo "Removing yad..."
-    sudo apt-get remove;
+echo "## Verifying Yad Installation"
+YAD_OK=$(dpkg-query -W --showformat='${Status}\n' yad)
+
+if [ "" != "$YAD_OK" ]; then
+    echo "Removing yad and dependencies..."
+    sudo apt-get remove -y yad && sudo apt-get autoremove -y
 fi
 
 echo "## Removing Binary Files"
 rm -rf $INSTALL_FOLDER
 
 echo "## Removing Reminder Theme"
-echo "Removing icons/themes folders..."
 sudo rm -rf $TO_THEMES_FOLDER
 sudo rm -rf $TO_ICONS_FOLDER
-echo "Adding Yaru-Grey-dark theme..."
-sudo rm -rf $FROM_THEMES_FOLDER $TO_THEMES_FOLDER
-sudo rm -rf $FROM_ICONS_FOLDER $TO_ICONS_FOLDER
 
 echo "## Removing Bashrc Configs & Symbolic Links"
-echo "Removing Non-network local connections to access control list...";
+echo "Removing Non-network local connections to access control list..."
 sed -i '/^xhost local.*/d' ~/.bashrc
-echo "Removing symbolic links...";
+echo "Removing symbolic links..."
 rm $BIN_FOLDER/remind
 rm $BIN_FOLDER/rremind
-echo "# Reminder Uninstallation Complete!";
+echo "# Reminder Uninstallation Complete!"
